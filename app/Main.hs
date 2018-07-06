04c0c83ec4b5064388f6f1241c2bfc9bf2263a2b
Yusuke Sangenya <longinus.eva@gmail.com>
2018-07-06 19:54:33 +0900

    Introduce config.yaml

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-} 
 {-# LANGUAGE ScopedTypeVariables #-} 
+{-# LANGUAGE DeriveGeneric #-}
 
 module Main where
 
 import Lib (createAtomFeed, createRss2Feed, composeFeeds)
 
-import Text.Feed.Import (parseFeedFromFile)
+import Text.Feed.Types (Feed)
+import Text.Feed.Import (parseFeedSource)
 import Text.Atom.Feed.Export (xmlFeed)
 import qualified Text.Atom.Feed as AF
 import Text.RSS.Export (xmlRSS)
 import qualified Text.RSS.Syntax as R2S
 
+import qualified Data.Text as T
 import qualified Data.Text.Lazy.IO as LTI
 import qualified Text.XML as C
 import Data.XML.Types as XML
 import qualified Data.Text.Lazy as Lazy
 import Data.Maybe (fromJust)
+import Control.Monad
 import Control.Monad.IO.Class
-
+import Network.HTTP.Simple
+import GHC.Generics
 import Web.Scotty
+import qualified Data.Yaml as Y
 
 import Paths_deruta
 
@@ -32,21 +38,23 @@ renderAtomFeed = fmap (C.renderText C.def) . elementToDoc . xmlFeed
 renderRss2Feed :: R2S.RSS -> Maybe Lazy.Text
 renderRss2Feed = fmap (C.renderText C.def) . elementToDoc . xmlRSS
 
+fetchFeeds :: [String] -> IO [Feed]
+fetchFeeds links = forM links $ \link -> fmap (fromJust . parseFeedSource . getResponseBody) $ httpLBS =<< parseRequest link
+
+data Config = Config { sources :: [String], feedId :: T.Text, feedTitle :: T.Text } deriving Generic
+instance Y.FromJSON Config
+
 main :: IO ()
-main = scotty 3000 $ do
-    get "/feed" $ do
-        feeds <- liftIO $ mapM (\f -> parseFeedFromFile =<< getDataFileName f) ["files/genya0407-atom.xml", "files/genya0407-rss.xml"]
-        let newAtomFeedId = "http://example.com"
-        let newAtomFeedTitle = "Example feed"
-        let composedAtomFeed = createAtomFeed newAtomFeedId newAtomFeedTitle . composeFeeds $ feeds
-        let feed_xml_text = fromJust $ renderAtomFeed composedAtomFeed
-        text feed_xml_text
-        setHeader "Content-Type" "text/xml"
-    get "/rss" $ do
-        feeds <- liftIO $ mapM (\f -> parseFeedFromFile =<< getDataFileName f) ["files/genya0407-atom.xml", "files/genya0407-rss.xml"]
-        let newAtomFeedId = "http://example.com"
-        let newAtomFeedTitle = "Example RSS"
-        let composedRss2Feed = createRss2Feed newAtomFeedId newAtomFeedTitle . composeFeeds $ feeds
-        let feed_xml_text = fromJust $ renderRss2Feed composedRss2Feed
-        text feed_xml_text
-        setHeader "Content-Type" "text/xml"
+main = do
+    (config :: Config) <- Y.decodeFileThrow "./config.yaml"
+    scotty 3000 $ do
+        get "/feed" $ do
+            feeds <- liftIO $ fetchFeeds . sources $ config
+            let composedAtomFeed = createAtomFeed (feedId config) (feedTitle config) . composeFeeds $ feeds
+            text . fromJust . renderAtomFeed $ composedAtomFeed
+            setHeader "Content-Type" "text/xml"
+        get "/rss" $ do
+            feeds <- liftIO $ fetchFeeds . sources $ config
+            let composedRss2Feed = createRss2Feed (feedId config) (feedTitle config) . composeFeeds $ feeds
+            text . fromJust . renderRss2Feed $ composedRss2Feed
+            setHeader "Content-Type" "text/xml"
