14e798f43e7c8a96acdcab77ab0f31a548527ce3
Yusuke Sangenya <longinus.eva@gmail.com>
2018-07-06 11:50:29 +0900

    Change property name.

--- app/Main.hs
+++ app/Main.hs
@@ -23,40 +23,41 @@ parseTimeStringISO8601 = parseTimeM True defaultTimeLocale "%Y-%m-%dT%H:%M:%S%z"
 parseTimeStringRFC822 :: String -> Maybe UTCTime
 parseTimeStringRFC822 = parseTimeM True defaultTimeLocale rfc822DateFormat
 
-data CommonItem = CommonItem { title :: Text, url :: Text, author :: Text, published :: UTCTime } deriving Show
+data CommonItem = CommonItem { itemTitle :: Text, itemUrl :: Text, itemAuthor :: Text, itemPublished :: UTCTime } deriving Show
 
 fromAtomEntry :: AF.Entry -> Maybe CommonItem
 fromAtomEntry entry = do
-    let t = (pack . AF.txtToString . AF.entryTitle) entry
-    u <- ((fmap AF.linkHref) . listToMaybe . AF.entryLinks) entry
-    a <- ((fmap AF.personName) . listToMaybe . AF.entryAuthors) entry
-    pText <- AF.entryPublished entry
-    p <- (parseTimeStringISO8601 . unpack) pText
-    return CommonItem { title = t , url = u, author = a , published = p }
+    let title = (pack . AF.txtToString . AF.entryTitle) entry
+    url <- ((fmap AF.linkHref) . listToMaybe . AF.entryLinks) entry
+    author <- ((fmap AF.personName) . listToMaybe . AF.entryAuthors) entry
+    publishedText <- AF.entryPublished entry
+    published <- (parseTimeStringISO8601 . unpack) publishedText
+
+    return CommonItem { itemTitle = title , itemUrl = url, itemAuthor = author , itemPublished = published }
 
 fromRss2Entry :: R2S.RSSItem -> Maybe CommonItem
 fromRss2Entry item = do
-    t <- R2S.rssItemTitle item
-    u <- R2S.rssItemLink item
-    a <- R2S.rssItemLink item
-    pText <- R2S.rssItemPubDate item
-    p <- parseTimeStringRFC822 . unpack $ pText
+    title <- R2S.rssItemTitle item
+    url <- R2S.rssItemLink item
+    author <- R2S.rssItemLink item
+    publishedText <- R2S.rssItemPubDate item
+    published <- parseTimeStringRFC822 . unpack $ publishedText
 
-    return CommonItem { title = t, url = u, author = a, published = p }
-
-main :: IO ()
-main = do
-    feeds <- mapM (\f -> parseFeedFromFile =<< getDataFileName f) ["files/genya0407-atom.xml", "files/genya0407-rss.xml"]
-    let commonItems = composeFeeds feeds
-    forM (sortBy (\l r -> (published l) `compare` (published r)) commonItems) $ \item -> do
-        TI.putStr . title $ item
-        TI.putStr "("
-        putStr . show . published $ item
-        TI.putStrLn ")"
-    return ()
+    return CommonItem { itemTitle = title , itemUrl = url, itemAuthor = author , itemPublished = published }
 
 composeFeeds :: [Feed] -> [CommonItem]
 composeFeeds [] = []
 composeFeeds ((AtomFeed feed):rest) = (catMaybes . map fromAtomEntry . AF.feedEntries $ feed) ++ (composeFeeds rest)
 composeFeeds ((RSSFeed feed):rest) = (catMaybes . map fromRss2Entry . R2S.rssItems . R2S.rssChannel $ feed) ++ (composeFeeds rest)
 composeFeeds _ = []
+
+main :: IO ()
+main = do
+    feeds <- mapM (\f -> parseFeedFromFile =<< getDataFileName f) ["files/genya0407-atom.xml", "files/genya0407-rss.xml"]
+    let commonItems = composeFeeds feeds
+    forM (sortBy (\l r -> (itemPublished l) `compare` (itemPublished r)) commonItems) $ \item -> do
+        TI.putStr . itemTitle $ item
+        TI.putStr "("
+        putStr . show . itemPublished $ item
+        TI.putStrLn ")"
+    return ()
